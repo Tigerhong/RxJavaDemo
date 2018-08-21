e8f8cbbd520cd8e7b48bb09c357a74737df26403
hongzhengong <hongzhengonghzg@gmail.com>
2018-08-21 14:39:10 +0800

    【功能】 最可爱猫获取功能1版,当保存最可爱猫失败时，返回一个默认值

--- LearnRxThought/src/main/java/com/example/learnrxthought/CatsHelper.java
+++ LearnRxThought/src/main/java/com/example/learnrxthought/CatsHelper.java
@@ -1,7 +1,5 @@
 package com.example.learnrxthought;
 
-import com.sun.jndi.toolkit.url.Uri;
-
 import java.util.Collections;
 import java.util.List;
 
@@ -14,6 +12,7 @@ import java.util.List;
 public class CatsHelper {
 
     Api api;
+    private String someDefaultValue="服务器上默认最可爱猫的地址";
 
     /**
      *保存最可爱的猫，并将其服务器路径返回
@@ -21,10 +20,15 @@ public class CatsHelper {
      * @return
      */
     public String saveTheCutestCat(String query){
-        List<Cat> cats = api.queryCats(query);
-        Cat cutest = findCutest(cats);
-        String savedUri = api.store(cutest);
-        return savedUri;
+        try {
+            List<Cat> cats = api.queryCats(query);
+            Cat cutest = findCutest(cats);
+            String savedUri = api.store(cutest);
+            return savedUri;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return someDefaultValue;
+        }
     }
 
     /**
