d05a3ca5c291ab2b9356d7823ca6e91f3e2c46ad
hongzhengong <hongzhengonghzg@gmail.com>
2018-11-13 10:27:53 +0800

    【功能】 增加功能打印123

---
+++ TestMerge/src/main/java/com/example/testmerge/Demo.java
@@ -0,0 +1,13 @@
+package com.example.testmerge;
+
+/**
+ * Description: Demo
+ * Date: 2018/11/13
+ *
+ * @author zhengong.hong@ubtrobot.com
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
