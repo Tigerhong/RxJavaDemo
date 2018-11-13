fff70f1dba0524ca00b5c1fa1ad98fe70603a478
hongzhengong <hongzhengonghzg@gmail.com>
2018-11-13 11:13:59 +0800

    Revert "【功能】 增加功能打印4"
    This reverts commit 9566a6f

--- TestMerge/src/main/java/com/example/testmerge/Demo.java
+++ TestMerge/src/main/java/com/example/testmerge/Demo.java
@@ -8,8 +8,6 @@ package com.example.testmerge;
  */
 public class Demo {
     public static void main(String[] args) {
-        System.out.println(4);
-        System.out.println(2);
         System.out.println(123);
     }
 }
