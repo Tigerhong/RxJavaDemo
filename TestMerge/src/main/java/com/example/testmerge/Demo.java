136ab619d52d977449cd9d944bbe85e9a492e11b
hongzhengong <hongzhengonghzg@gmail.com>
2018-11-13 10:32:59 +0800

    【功能】 增加功能打印2

--- TestMerge/src/main/java/com/example/testmerge/Demo.java
+++ TestMerge/src/main/java/com/example/testmerge/Demo.java
@@ -8,6 +8,7 @@ package com.example.testmerge;
  */
 public class Demo {
     public static void main(String[] args) {
+        System.out.println(2);
         System.out.println(123);
         System.out.println(1);
     }
